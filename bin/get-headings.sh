350ce1780e1c6ac577d0762ecd71e5e1447da7e1
Mark Bucklin <markbucklin@gmail.com>
2019-05-16 19:56:22 -0400

    remove pdfs from draft-chapters dir and add amore optiosn to get-headings.sh file

--- bin/get-headings.sh
+++ bin/get-headings.sh
@@ -4,6 +4,7 @@ mdfile="$1"
 
 sed -n '/^#.*\w$/p' "$mdfile"
 
-# or use look '# ' "$mdfile"
+sed -n '/^\(#\{1,6\}\) \(.*\)$/p' *.md
 
+# or use look '# ' "$mdfile
 # fd -t f -e md | xargs -r sed -n '/^#.*\w$/p'
